ae34f43fc83b2a2185e257c97337166a6e03616a
Skander228 <kkhemiriskander@gmail.com>
2022-05-04 17:31:52 +0300

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,5 @@
 package com.company;
 
-<<<<<<< HEAD
-import java.lang.reflect.Array;
-=======
->>>>>>> fa271ac (Initial commit)
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,28 +10,14 @@ public class Main {
         int largestSum = 0;
         for(int i=0; i<6; i++){
             numArray[i]=sc.nextLine().split(" ");
-            System.out.println(Arrays.deepToString(numArray));
         }
 
         for(int i=0;i<=3;i++){
             for(int j=0;j<=3;j++){
-<<<<<<< HEAD
-                //0 0 0 1 1 1   0+0+0 1
-                //0 1 0 1 0 1
-                //2 2 2 1 1 1
-                //2 2 1 1 2 2
-                //0 2 0 1 0 1
-                //1 1 2 2 3 1
                 int sum = Integer.parseInt(numArray[i][j])+Integer.parseInt(numArray[i][j+1])
                         +Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])
                         +Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])
                         +Integer.parseInt(numArray[i+2][j+2]);
-=======
-                int sum = Integer.parseInt(numArray[i][j])+Integer.parseInt(numArray[i][j+1])+
-                        Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])+
-                        Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])+
-                        Integer.parseInt(numArray[i+2][j+2]);
->>>>>>> fa271ac (Initial commit)
                 if(i==0 && j==0){
                     largestSum=sum;
                 }else{
