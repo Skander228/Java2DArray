3d8e3e441da759ad03176a9f2da9a5bffa0f9207
Skander228 <kkhemiriskander@gmail.com>
2022-05-04 17:20:38 +0300

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+<<<<<<< HEAD
 import java.lang.reflect.Array;
+=======
+>>>>>>> fa271ac (Initial commit)
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,6 +19,7 @@ public class Main {
 
         for(int i=0;i<=3;i++){
             for(int j=0;j<=3;j++){
+<<<<<<< HEAD
                 //0 0 0 1 1 1   0+0+0 1
                 //0 1 0 1 0 1
                 //2 2 2 1 1 1
@@ -26,6 +30,12 @@ public class Main {
                         +Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])
                         +Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])
                         +Integer.parseInt(numArray[i+2][j+2]);
+=======
+                int sum = Integer.parseInt(numArray[i][j])+Integer.parseInt(numArray[i][j+1])+
+                        Integer.parseInt(numArray[i][j+2])+Integer.parseInt(numArray[i+1][j+1])+
+                        Integer.parseInt(numArray[i+2][j])+Integer.parseInt(numArray[i+2][j+1])+
+                        Integer.parseInt(numArray[i+2][j+2]);
+>>>>>>> fa271ac (Initial commit)
                 if(i==0 && j==0){
                     largestSum=sum;
                 }else{
